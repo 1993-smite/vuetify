6dd7233036471b653c0aea25e7f457f990ecc5f0
Alex Sasov <sasov1993@mail.ru>
2020-12-03 23:10:02 +0300

    добавил таблицу

--- src/components/views/Test1.vue
+++ src/components/views/Test1.vue
@@ -1,19 +1,61 @@
 <template>
-    <v-row justify="center">
-        <label>{{getUserName}}</label><dUser></dUser>
 
-    </v-row>
+  <div>
+    <v-tabs
+        v-model="tab" fixed-tabs>
+
+        <v-tabs-slider color="yellow"></v-tabs-slider>
+
+        <v-tab
+          ripple
+        >
+          Диалог
+        </v-tab>
+        <v-tab
+          ripple
+        >
+          Таблица
+        </v-tab>
+
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <v-card flat>
+          <v-spacer></v-spacer>
+          <v-row justify="center">
+            <label>{{getUserName}}</label>
+          </v-row>
+          <v-row justify="center">
+            <dUser></dUser>
+          </v-row>
+        </v-card>
+      </v-tab-item>
+
+      <v-tab-item>
+        <v-card flat>
+          <v-spacer></v-spacer>
+          <v-row justify="center">
+            <Table></Table>
+          </v-row>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
+
 </template>
 
 <script>
 
-  import dUser from './dUser.vue'
+  import dUser from './tests/dUser'
+  import Table from './tests/Table'
 
 export default {
   name: 'Test1',
 
   components: {
-    dUser
+    dUser,
+    Table
   },
 
   computed: {
@@ -26,7 +68,7 @@ export default {
   },
 
   data: () => ({
-    //
+    tab: null,
   }),
 
   mounted(){
